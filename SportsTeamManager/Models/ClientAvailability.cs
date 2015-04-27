8fd463a110c998a8422956a65ba735b0093903f9
Ruth McElroy <ruthie_mac_@hotmail.com>
2015-04-27 15:17:09 +0100

    match id in client availability

--- SportsTeamManager/Models/ClientAvailability.cs
+++ SportsTeamManager/Models/ClientAvailability.cs
@@ -7,12 +7,13 @@ namespace SportsTeamManager.Models
 {
     public class ClientAvailability
     {
-        
-        public int ID{get;set;}
-        public string Opposition{get;set;}
-        public string Name{get; set;}
-        public DateTime Time {get;set;}
-        public bool Available{get;set;}
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MatchId { get; set; }
+        public string Opposition { get; set; }
+        public DateTime Time { get; set; }
+        public bool Available { get; set; }
 
     }
 }
